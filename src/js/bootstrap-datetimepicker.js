1e92b06bc05170657460da4037f5bf7f1ee69ce3
Julian Popescu <jpopesculian@gmail.com>
2017-03-23 16:25:29 -0400

    version bump to 4.17.49

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -1,4 +1,4 @@
-/*! version : 4.17.48
+/*! version : 4.17.49
  =========================================================
  bootstrap-datetimejs
  https://github.com/Eonasdan/bootstrap-datetimepicker
